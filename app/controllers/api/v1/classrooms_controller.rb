fc658150cb35787e1a0ed68d8f2d33a659ea7acd
Tiberiu Frățilă <tiberiu.fratila@gmail.com>
2021-03-16 01:44:10 +0200

    add classrooms api (v1)

---
+++ app/controllers/api/v1/classrooms_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::ClassroomsController < Api::V1::BaseController
+    def index
+      if params[:school_id]
+        @classrooms = Classroom.where(school_id: params[:school_id])
+      else
+        @classrooms = Classroom.all
+      end
+
+      render(json: @classrooms)
+    end
+  
+    def show
+      @classroom = Classroom.find(params[:id])
+  
+      render(json: @classroom.to_json)
+    end
+  end
